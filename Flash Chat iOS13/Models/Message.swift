1dd4f43057243d6b1301f5eedb1249ada25962ba
MelihYuvaci <melihyuvaci@icloud.com>
2023-05-11 12:46:16 +0300

    Initial commit

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Melih Yuvacı on 23.10.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String // 1@2.com
+    let body: String
+}
